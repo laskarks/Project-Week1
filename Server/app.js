e0604d075708bc0ad592ca804b45f4d3bb9a1f22
mardiansn <mardiansyah.011@gmail.com>
2019-10-03 18:51:37 +0700

    first commit

--- Server/app.js
+++ Server/app.js
@@ -1,4 +1,4 @@
-if(process.env.NODE_ENV==='development'){
+if (process.env.NODE_ENV === 'development') {
     require('dotenv').config()
 }
 
@@ -7,20 +7,20 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const morgan = require('morgan')
-//Moongo connection
+    //Moongo connection
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/grup-project1', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost:27017/group-project1', { useNewUrlParser: true });
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
     console.log('We Are Connected')
 });
-//---> mongoose Connection
-app.use(morgan('dev'))
-app.use(express.urlencoded({extended: false}));
-app.use(express.json())
+//---> Middleware initials
 app.use(cors())
+app.use(morgan('dev'))
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
 
 //Your Code add below this----
 
@@ -35,9 +35,4 @@ app.use(cors())
 
 
 
-app.listen(PORT,() => console.log(`Server Running on ${PORT}`))
-
-
-
-
-
+app.listen(PORT, () => console.log(`Server Running on ${PORT}`))
